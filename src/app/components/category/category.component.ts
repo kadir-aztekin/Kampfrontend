0d0d3c10abf7c13d5f4a18ed4f632e9a641a308d
KADİR AZTEKİN <76233680+kadir-aztekin@users.noreply.github.com>
2021-03-17 01:48:59 +0300

    COMMİT1

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -5,41 +5,38 @@ import { CategoryService } from 'src/app/services/category.service';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
-
-  categories:Category[]=[];
-  currenCategory:Category;
-  dataLoaded=false;
-  constructor(private categoryService:CategoryService) { }
+  categories: Category[] = [];
+  currenCategory: Category;
+  dataLoaded = false;
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
-    this.getCategories()
+    this.getCategories();
   }
-  getCategories(){
-    this.categoryService.getCategories().subscribe(response =>{
-      this.categories = response.data
-      this.dataLoaded=true;
-      
-    })
+  getCategories() {
+    this.categoryService.getCategories().subscribe((response) => {
+      this.categories = response.data;
+      this.dataLoaded = true;
+    });
   }
-  setCurrentCategory(category:Category){
-    this.currenCategory=category;
+  setCurrentCategory(category: Category) {
+    this.currenCategory = category;
   }
-  getCurrentCategoryClass(category:Category){
-    if(category==this.currenCategory){
-      return "list-group-item active"
-    }else{
-      return "list-group-item"
+  getCurrentCategoryClass(category: Category) {
+    if (category == this.currenCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
     }
   }
-  getAllCategoryClass(){
-    if(!this.currenCategory){
-      return "list-group-item active"
-    }else{
-      return "list-group-item"
+  getAllCategoryClass() {
+    if (!this.currenCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
     }
   }
-
 }
