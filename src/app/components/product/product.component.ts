a95e9832a06ce11ac4bbd6e428ce14ccdcffec5a
KADİR AZTEKİN <76233680+kadir-aztekin@users.noreply.github.com>
2021-03-21 01:21:20 +0300

    20.ders commit

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -10,31 +12,45 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
-  dataLoaded =false;
+  dataLoaded = false;
+  filterText="";
   
-  constructor(private productService:ProductService,private activatedRoute:ActivatedRoute) {}
+  constructor(private productService:ProductService, 
+    private activatedRoute:ActivatedRoute, 
+    private toastrService:ToastrService,
+    private cartService:CartService) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params=>{
-      if(params["categoryId"]){
-        this.getProductsByCategory(params["categoryId"])
-      }else{
-        this.getProducts()
-      }
-    })
+      this.activatedRoute.params.subscribe(params=>{
+        if(params["categoryId"]){
+          this.getProductByCategory(params["categoryId"])
+        }else{
+          this.getProducts()
+        }
+      })
   }
 
   getProducts() {
-    this.productService.getProducts().subscribe(response =>{
+    this.productService.getProducts().subscribe(response=>{
       this.products = response.data
-      this.dataLoaded=true;
-      
-    })
+      this.dataLoaded = true;
+    })   
   }
-  getProductsByCategory(categoryId:number) {
-    this.productService.getProductByCategory(categoryId).subscribe(response =>{
+
+  getProductByCategory(categoryId:number) {
+    this.productService.getProductByCategory(categoryId).subscribe(response=>{
       this.products = response.data
-      this.dataLoaded=true;
-    })
+      this.dataLoaded = true;
+    })   
+  }
+
+  addToCart(product:Product){
+      if(product.productId===1){
+        this.toastrService.error("Hata","Bu ürün sepete eklenemez")
+      }else{
+        this.toastrService.success("Sepete eklendi",product.productName)
+        this.cartService.addToCart(product);
+      }
+   
   }
 }
